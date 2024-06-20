f45fabcb1071c4e73c14b76c03b532bce312fa56
Mauricio <mauriciobraga100@gmail.com>
2024-06-20 11:20:09 -0300

    funcionalidade de logout implementada

--- frontend/src/actions/login.ts
+++ frontend/src/actions/login.ts
@@ -22,19 +22,44 @@ export async function login(data: { emailOuCpf: string; senha: string; rememberM
       },
     );
     const dados = response.data;
-    const maxAge = data.rememberMe ? 604800 : 14400; // 7 dias se lembrar, senão 4 horas
-    cookies().set("token", dados.token, {
+
+    const cookieOptions: any = {
+      name: "token",
+      value: dados.token,
       httpOnly: true,
       secure: true,
       sameSite: "lax",
-      maxAge: maxAge,
-    });
+    };
+
+    if (data.rememberMe) {
+      cookieOptions.maxAge = 604800; // 7 dias
+    }
+
+    cookies().set(cookieOptions);
   } catch (error: any) {
     console.error(error.response?.data);
 
     return {
       success: false,
       message: apiErro(error),
+    };
+  }
+}
+
+export async function logout() {
+  try {
+    cookies().delete({
+      name: "token",
+      httpOnly: true,
+      secure: true,
+      sameSite: "lax",
+    });
+    return { success: true };
+  } catch (error: any) {
+    console.error(error);
+    return {
+      success: false,
+      message: "Erro ao tentar sair",
     };
   }
 }
