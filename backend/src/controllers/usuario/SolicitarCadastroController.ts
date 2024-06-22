4418c3f601d3a1bad86c9ba961d04b1564be5f52
Mauricio <mauriciobraga100@gmail.com>
2024-06-21 23:24:35 -0300

    Tipo usuário solicitar cadastro

--- backend/src/controllers/usuario/SolicitarCadastroController.ts
+++ backend/src/controllers/usuario/SolicitarCadastroController.ts
@@ -3,14 +3,14 @@ import { Request, Response } from "express";
 
 export class SolicitarCadastroController {
   async handle(req: Request, res: Response) {
-    const { email, tipo_usuario } = req.body;
+    const { email, tipo } = req.body;
 
     const idUsuario = req.id_usuario;
 
     try {
       const solicitarCadastroService = new SolicitarCadastroService();
 
-      await solicitarCadastroService.execute(idUsuario, email, tipo_usuario);
+      await solicitarCadastroService.execute(idUsuario, email, tipo);
       res.status(200).json({ message: "E-mail para realizar o cadastro enviado com sucesso!" });
     } catch (error) {
       if (error.message === "Usuário já existe.") {
