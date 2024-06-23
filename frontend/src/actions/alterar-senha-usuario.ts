2d889b7b141d2ecf0f743de8ccff7815ea8231d8
Mauricio <mauriciobraga100@gmail.com>
2024-06-23 02:19:27 -0300

    Formulários ok

---
+++ frontend/src/actions/alterar-senha-usuario.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import { apiErro } from "@/api/api-erros";
+import { EDITAR_USUARIO } from "@/api/endpoints";
+import { verificarToken } from "@/scripts/verificarToken";
+import axios from "axios";
+import { cookies } from "next/headers";
+
+export async function alterarSenhaUsuario(data: { senha: string; novaSenha: string }) {
+  const cookieData = cookies().get("token");
+  const token = cookieData ? cookieData.value : null;
+  console.log("Token do usuário", token);
+
+  if (!token || !(await verificarToken(token))) {
+    console.error("Token inválido ou expirado.");
+    return {
+      success: false,
+      message: "Token inválido ou expirado.",
+    };
+  }
+
+  try {
+    const { url } = EDITAR_USUARIO();
+    const response = await axios.put(url, data, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return {
+      success: true,
+      message: response.data.message,
+    };
+  } catch (error: unknown) {
+    console.log(error);
+
+    return {
+      success: false,
+      message: apiErro(error),
+    };
+  }
+}
