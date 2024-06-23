0cc35ad5ff2834ea7c539e4da73c77647a9104f1
Mauricio <mauriciobraga100@gmail.com>
2024-06-23 01:20:59 -0300

    footer

--- frontend/src/components/dashboard/footer.tsx
+++ frontend/src/components/dashboard/footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <div className="supports-backdrop-blur:bg-background/60 z-20 w-full shadow bg-background/95 backdrop-blur">
       <div className="mx-4 md:mx-8 flex h-14 items-center">
-        <p className="text-xs md:text-sm leading-loose text-muted-foreground text-left">
+        <p className="text-xs md:text-xs leading-loose text-muted-foreground text-left">
           &copy; {date.getFullYear()} LABED - Laboratório de Eletrônica e Desenvolvimento
         </p>
       </div>
