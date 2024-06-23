9c8351aa0b049fa00c27f59b140dbe402d65be4e
Mauricio <mauriciobraga100@gmail.com>
2024-06-23 01:17:34 -0300

    copy footer

--- frontend/src/components/dashboard/footer.tsx
+++ frontend/src/components/dashboard/footer.tsx
@@ -1,9 +1,11 @@
 export function Footer() {
+  const date = new Date();
+
   return (
     <div className="supports-backdrop-blur:bg-background/60 z-20 w-full shadow bg-background/95 backdrop-blur">
       <div className="mx-4 md:mx-8 flex h-14 items-center">
         <p className="text-xs md:text-sm leading-loose text-muted-foreground text-left">
-          © 2024 LABED - Laboratório de Eletrônica e Desenvolvimento.
+          &copy; {date.getFullYear()} LABED - Laboratório de Eletrônica e Desenvolvimento
         </p>
       </div>
     </div>
