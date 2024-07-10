3aa3120b48cb9b3a84263a3ddfbdadad0de300a5
Bernardo Viero <bernardo.viero@avmb.com.br>
2024-07-10 17:03:03 -0300

    testes

--- backend/src/services/calculos/analise-qualitativa/CalcularAnaliseQualitativaService.ts
+++ backend/src/services/calculos/analise-qualitativa/CalcularAnaliseQualitativaService.ts
@@ -1,43 +1,28 @@
-import { mean } from "mathjs";
-
 export class CalcularAnaliseQualitativaService {
-  private extrairValoresSubpropriedade(obj: any, propriedade: string): number[] {
-    const valores: number[] = [];
+  private calcularEscore(valorA: number, valorB: number): number {
+    return valorA + valorB / 2;
+  }
 
-    const extrair = (obj: any) => {
-      for (const key in obj) {
-        if (key === propriedade && typeof obj[key] === "number") {
-          valores.push(obj[key]);
-        } else if (typeof obj[key] === "object" && obj[key] !== null) {
-          extrair(obj[key]);
+  private buscarValorSubpropriedade(objetos: any[], propriedade: string, subpropriedade: string) {
+    const valorA = 0;
+    const valorB = 0;
+    objetos.forEach((objeto) => {
+      objeto[propriedade].forEach((subpropriedadeObjeto) => {
+        valorA = 
         }
       }
-    };
-
-    extrair(obj);
-    return valores;
-  }
-
-  private calcularMedia(valores: number[]): number {
-    if (valores.length === 0) return 0;
-    return parseFloat(mean(valores).toFixed(2));
-  }
-
-  private calcularEstatisticasSubpropriedades(entities: any[], propriedade: string) {
-    const valores = entities
-      .flatMap((entity) => this.extrairValoresSubpropriedade(entity.percepcao_risco, propriedade))
-      .filter((value) => typeof value === "number");
-
-    return {
-      media: this.calcularMedia(valores),
-    };
+    });
   }
 
   async execute(informacoes: JSON) {
-    let informacoesCalculadas: any = {};
+    const informacoesCalculadas: object = {};
 
     try {
-      informacoesCalculadas = informacoes;
+      informacoesCalculadas["lideranca"] = {
+        gerentes_fornecem_assistencia_orientacao_seguranca_alimentos: {
+          escore: 4,
+        },
+      };
     } catch (error) {
       console.error(error.message);
       throw new Error(error.message);
