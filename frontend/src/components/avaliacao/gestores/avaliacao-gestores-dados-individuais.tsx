88e4118b2456a56780947c8c8c23313297712384
Mauricio <mauriciobraga100@gmail.com>
2024-07-20 02:21:54 -0300

    verificações campos gestor

--- frontend/src/components/avaliacao/gestores/avaliacao-gestores-dados-individuais.tsx
+++ frontend/src/components/avaliacao/gestores/avaliacao-gestores-dados-individuais.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { useState } from "react";
 import { Input } from "../../ui/input";
 import { Label } from "../../ui/label";
 import { RadioGroup, RadioGroupItem } from "../../ui/radio-group";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "../../ui/select";
 
 export function AvaliacaoGestoresDadosIndividuais() {
+  const [escolaridade, setEscolaridade] = useState("");
+  const [realizaTreinamentos, setRealizaTreinamentos] = useState(false);
+
   return (
     <>
       <h3 className="mb-8 text-lg">Dados Pessoais</h3>
@@ -26,12 +30,12 @@ export function AvaliacaoGestoresDadosIndividuais() {
 
         <div>
           <Label>Idade</Label>
-          <Input type="number" />
+          <Input type="number" placeholder="Digite a idade do gestor" />
         </div>
 
         <div>
           <Label>Escolaridade</Label>
-          <Select>
+          <Select onValueChange={(value) => setEscolaridade(value)}>
             <SelectTrigger>
               <SelectValue placeholder="Selecione uma opção" />
             </SelectTrigger>
@@ -48,10 +52,12 @@ export function AvaliacaoGestoresDadosIndividuais() {
           </Select>
         </div>
 
-        <div>
-          <Label>Formação</Label>
-          <Input />
-        </div>
+        {(escolaridade === "5" || escolaridade === "6") && (
+          <div>
+            <Label>Formação</Label>
+            <Input placeholder="Curso que é formado ou em andamento" />
+          </div>
+        )}
 
         <div>
           <Label>Participou de treinamento para manipulação de alimentos?</Label>
@@ -68,8 +74,8 @@ export function AvaliacaoGestoresDadosIndividuais() {
         </div>
 
         <div>
-          <Label>Há quanto tempo trabalha com alimentos? (meses)</Label>
-          <Input type="number" />
+          <Label>Há quanto tempo trabalha com alimentos?</Label>
+          <Input type="number" placeholder="Número de meses trabalhados" />
         </div>
 
         <div>
@@ -90,48 +96,47 @@ export function AvaliacaoGestoresDadosIndividuais() {
           <Label>Você realiza treinamentos com os funcionários a respeito de boas práticas de manipulação?</Label>
           <RadioGroup className="flex gap-2">
             <div className="flex items-center space-x-2">
-              <RadioGroupItem value="1" id="sim" />
+              <RadioGroupItem value="1" id="sim" onClick={() => setRealizaTreinamentos(true)} />
               <Label htmlFor="sim">Sim</Label>
             </div>
             <div className="flex items-center space-x-2">
-              <RadioGroupItem value="0" id="nao" />
+              <RadioGroupItem value="0" id="nao" onClick={() => setRealizaTreinamentos(false)} />
               <Label htmlFor="nao">Não</Label>
             </div>
           </RadioGroup>
         </div>
 
-        <div>
-          <Label>Qual a frequência de aplicação?</Label>
-          <Input />
-        </div>
+        {realizaTreinamentos && (
+          <>
+            <div>
+              <Label>Qual a frequência de aplicação?</Label>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Selecione uma opção" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectItem value="1">Diário</SelectItem>
+                    <SelectItem value="2">Semanal</SelectItem>
+                    <SelectItem value="3">Quinzenal</SelectItem>
+                    <SelectItem value="4">Mensal</SelectItem>
+                    <SelectItem value="5">Trimestral</SelectItem>
+                    <SelectItem value="6">Semestral</SelectItem>
+                    <SelectItem value="7">Anual</SelectItem>
+                    <SelectItem value="8">A cada 2 anos</SelectItem>
+                    <SelectItem value="9">A cada 3 ou mais anos</SelectItem>
+                    <SelectItem value="10">Só fez uma vez</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
 
-        <div>
-          <Label>Qual a frequência de aplicação?</Label>
-          <Select>
-            <SelectTrigger>
-              <SelectValue placeholder="Selecione uma opção" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value="1">Diário</SelectItem>
-                <SelectItem value="2">Semanal</SelectItem>
-                <SelectItem value="3">Quinzenal</SelectItem>
-                <SelectItem value="4">Mensal</SelectItem>
-                <SelectItem value="5">Trimestral</SelectItem>
-                <SelectItem value="6">Semestral</SelectItem>
-                <SelectItem value="7">Anual</SelectItem>
-                <SelectItem value="8">A cada 2 anos</SelectItem>
-                <SelectItem value="9">A cada 3 ou mais anos</SelectItem>
-                <SelectItem value="10">Só fez uma vez</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
-
-        <div>
-          <Label>Quais os temas costuma abordar nos treinamentos?</Label>
-          <Input />
-        </div>
+            <div>
+              <Label>Quais os temas costuma abordar nos treinamentos?</Label>
+              <Input placeholder="Tema 1, Tema 2, Tema 3..." />
+            </div>
+          </>
+        )}
       </div>
     </>
   );
